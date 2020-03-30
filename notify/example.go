901c45de6bfae010cb112e8d082ce0af97e80386
jacky <jery1024@163.com>
2020-03-30 15:59:20 +0800

    init project

---
+++ notify/example.go
@@ -0,0 +1,117 @@
+package notify
+
+import (
+	"errors"
+	"fmt"
+	"net/url"
+	"strings"
+
+	"github.com/gorilla/websocket"
+	"github.com/jacky2478/mlog"
+)
+
+var (
+	v_client *websocket.Conn
+	closeCh  chan struct{}
+)
+
+func WSClientInit(strUrl, path string, recvFunc func(from, data string)) error {
+	u := url.URL{Scheme: strings.Split(strUrl, "://")[0], Host: strings.Split(strUrl, "://")[1], Path: path}
+	c, _, err := websocket.DefaultDialer.Dial(u.String(), nil)
+	if err != nil {
+		mlog.Errorf("WSClient Init failed, detail: %v", err.Error())
+		return err
+	}
+	//defer c.Close()
+
+	v_client = c
+
+	closeCh = make(chan struct{}, 0)
+	if recvFunc != nil {
+		go receive(recvFunc)
+	}
+	go wait()
+	return nil
+}
+
+func WSClientSend(msg string) error {
+	if v_client == nil {
+		return errors.New("WSClientSend failed with invalid websocket client")
+	}
+
+	if err := v_client.WriteMessage(websocket.TextMessage, []byte(msg)); err != nil {
+		mlog.Errorf("WSClientSend failed with websocket.Send message, detail: %v", err.Error())
+		return err
+	}
+	return nil
+}
+
+func WSClientClose() {
+	if closeCh != nil {
+		close(closeCh)
+	}
+}
+
+func wait() {
+	for {
+		select {
+		case <-closeCh:
+			err := v_client.WriteMessage(websocket.CloseMessage, websocket.FormatCloseMessage(websocket.CloseNormalClosure, ""))
+			if err != nil {
+				mlog.Errorf("wait failed with websocket.Close message, detail: %v", err.Error())
+				return
+			}
+		}
+	}
+}
+
+func receive(recvFunc func(from, data string)) {
+	for v_client != nil {
+		_, message, err := v_client.ReadMessage()
+		if err != nil {
+			mlog.Errorf("receive failed, detail: %v", err.Error())
+			break
+		}
+		// mlog.Infof("receive message: %v", string(message))
+
+		if recvFunc != nil {
+			recvFunc(v_client.RemoteAddr().String(), string(message))
+		}
+	}
+
+	if v_client == nil {
+		return
+	}
+	v_client.Close()
+}
+
+// the structre for push
+/*
+	{
+		// 用户ID
+		UserID string `json:"userid"`
+
+		// 当前触发信号
+		Signal string `json:"signal"`
+
+		// 上次触发信号
+		LastSignal string `json:"last_signal"`
+
+		// 由用户自定义订阅的触发规则
+		TriggerRule string `json:"trigger_rule"`
+	}
+*/
+// the push demo for user
+/*
+	{
+		"userid": "123456",
+		"signal": "4.73",
+		"last_signal": "-0.85",
+		"trigger_rule": "收益率上涨幅度大于等于5.00"
+	}
+*/
+func main() {
+	WSClientInit("wss://www.selfquant.com", "/api/subscribe?userid=123456&usage=notify", func(from, data string) {
+		fmt.Printf("%s, %s\n", from, data)
+	})
+}
---
+++ notify/example.py
@@ -0,0 +1,35 @@
+import websocket
+import json
+try:
+    import thread
+except ImportError:
+    import _thread as thread
+import time
+
+#   the push demo for user
+# 	{
+# 		"userid": "123456",
+# 		"signal": "4.73",
+# 		"last_signal": "-0.85",
+# 		"trigger_rule": "收益率上涨幅度大于等于5.00"
+# 	}
+def on_message(ws, message):
+    print(message)
+
+def on_error(ws, error):
+    print(error)
+
+def on_close(ws):
+    print("### closed ###")
+
+def on_open(ws):
+    pass
+
+if __name__ == "__main__":
+    websocket.enableTrace(True)
+    ws = websocket.WebSocketApp("wss://www.selfquant.com/api/subscribe?userid=123456&usage=notify",
+                              on_message = on_message,
+                              on_error = on_error,
+                              on_close = on_close)
+    ws.on_open = on_open
+    ws.run_forever()
